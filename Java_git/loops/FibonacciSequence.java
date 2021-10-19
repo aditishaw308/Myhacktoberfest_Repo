af147393a5a4d8c76d9c3e164b13172b3756a11e
Anjan Kumar Majee <92600394+godxpredator@users.noreply.github.com>
2021-10-19 22:19:14 +0530

    Update FibonacciSequence.java

--- Java_git/loops/FibonacciSequence.java
+++ Java_git/loops/FibonacciSequence.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class FibonacciSequence {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter the sequence length:-");
+		System.out.println("Enter the length:-");
 		int n=sc.nextInt();
-		sc.close();
 		int a=0,b=1,c;
 		for(int i=1;i<=n;i++) {
-			System.out.print( a +" ");
+			System.out.print( a +"  ");
 			c=a;
 			a=b;
 			b+=c;
+			sc.close();
 		}
 	}
 }
