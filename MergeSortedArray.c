59cea7b5f56a52013613aac84cf5836193c348b0
RuhiRaj <89976257+RuhiRaj@users.noreply.github.com>
2021-10-21 11:45:31 +0530

    code changes
    this my code i just change your code @aditishaw308

--- MergeSortedArray.c
+++ MergeSortedArray.c
@@ -1,52 +1,49 @@
 #include<stdio.h>
-int main(){
-    int n1,n2,i,j;
-    printf("Enter the length of First Sorted Array:-\n");
-    scanf("%d",&n1);
-    int sort1[n1];
-    printf("Enter the First array elements in incresing order:-\n");
-    for(i=0;i<n1;i++){
-        scanf("%d",&sort1[i]);
-    }
-    printf("Enter the length of Second Sorted Array:-\n");
-    scanf("%d",&n2);
-    int sort2[n2];
-    printf("Enter the Second array elements in incresing order:-\n");
-    for(i=0;i<n2;i++){
-        scanf("%d",&sort2[i]);
-    }
-    i=0;
-    j=0;
-    int k=0;
-    int n=(n1+n2);
-    int res[n];
-    while(i<n1 && j<n2){
-        if(sort1[i]<=sort2[j]){
-            res[k]=sort1[i];
-            k++;
-            i++;
-        }else{
-            res[k]=sort2[j];
-            k++;
-            j++;
-        }
-    }
-    if(i>=n1){
-        while(j<n2){
-            res[k]=sort2[j];
-            k++;
-            j++;
-        }
-    }else{
-        while(i<n1){
-            res[k]=sort1[i];
-            k++;
-            i++;
-        }
-    }
-    printf("Merged Sorted Array is:-\n");
-    for(i=0;i<k;i++){
-        printf("%d ",res[i]);
-    }
-    return 0;
+
+int merge(int [],int,int,int);	
+int main()
+{
+	int n,a[20],i;
+	printf("Enter Size of array that you want :");
+	scanf("%d",&n);
+	printf("Enter elements :");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+ mergeSort(a,1,10);
+ printf("sorted array :");
+ for(i=0;i<=n;i++)
+ printf("%d \n",a[i]);
+ printf("\n");
+
+ return 0;
+}
+
+int merge(int a[],int l,int mid,int h){
+	int i=l,j=mid+1,k=l;
+	int b[20];
+	while(i<=mid&&j<=h){
+		if(a[i]<a[j]){
+			b[k++]=a[i++];
+		}
+		else
+		   b[k++]=a[j++];
+	}
+	for(;i<=mid;i++)
+		b[k++]=a[i];
+	for(;j<=h;j++)
+	    b[k++]=a[j];
+	    
+	for(i=l;i<=h;i++)
+	a[i]=b[i];
+}
+	
+ int mergesort(int a[], int l, int h){
+	if(l<h){
+		 int mid;
+		 mid=(l+h)/2;
+		mergesort(a,l,mid);
+		mergesort(a,mid+1,h);
+		merge(a,l,mid,h);
+	}
 }
